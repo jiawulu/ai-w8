bd424ad5fadd1feda2d9be15db323bf4d720a0f2
wuzhong <wuzhong@taobao.com>
2018-10-22 14:32:27 +0800

    remove chinese encoding

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import os
 import sys
 print(sys.getdefaultencoding())
 
-sys.setdefaultencoding('utf8')
+# sys.setdefaultencoding('utf8')
 
 print('current working dir [{0}]'.format(os.getcwd()))
 w_d = os.path.dirname(os.path.abspath(__file__))
